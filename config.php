ee1f68c5bbb40bf590cecec285f5fbf2f0a582de
tintem <hung.seu@gmail.com>
2023-09-16 08:36:27 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define("HOST_DB", 'localhost');
+define("USER_DB", 'root');
+define("PASS_DB", '');
+define("DB", 'myweb');
+
+define ('ROOT', dirname(__FILE__));
+define('BASE_URL', 'http://hocphp.com/');
+define('UPLOAD_IMG', ROOT .'/images/book');
+
